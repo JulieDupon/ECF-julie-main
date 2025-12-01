fb93e2392faabb75b1ecc92343c82adaed74e1fb
JulieDupon <j.dupon18@gmail.com>
2025-12-01 16:34:40 +0100

    partie html

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+let film = [
+    {
+        title: "La ligne verte",
+        years: 2000,
+        authors: "Franck Darabont"
+    },
+    {
+        title: "Le Roi Lion",
+        years: 1994,
+        authors: "Roger Allers et Rob Minkoff"
+    },
+    {
+        title: "Gladiator",
+        years: 2000,
+        authors: "Ridley Scott"
+    },
+    {
+        title: "Avatar",
+        years: 2009,
+        authors: "James Cameron"
+    }
+];
+console.log(film);
+
+let titre = document.getElementById("titre")
+let annee = document.getElementById("annee")
+let realisateur = document.getElementById("realisateur")
+
+
+const button = document.getElementById("ajouter"); 
+button.addEventListener("click", (e) => {
+      if () 
+});

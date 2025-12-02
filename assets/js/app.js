5764b4bdc232f71f885f53e538969fd568d97a7e
JulieDupon <j.dupon18@gmail.com>
2025-12-02 15:30:03 +0100

    ecf

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,37 @@ let annee = document.getElementById("annee")
 let realisateur = document.getElementById("realisateur")
 
 
-const button = document.getElementById("ajouter"); 
-button.addEventListener("click", (e) => {
-      if () 
+const button = document.getElementById("ajouter");//vérification des données de l'utilisateur
+button.addEventListener("click", () => {
+    if(titre.lengt < 2 ){
+        alert("Le titre doit avoir au moins 2 caractères!");
+        preventDefault();
+        return false;
+    }
+    if(annee.length < 4){
+        alert("L'année doit contenir que 4 chiffres!");
+        preventDefault();
+        return false;
+    }
+    if(realisateur.length < 5 ){
+        alert("Le réalisateur doit avoir minimum 5 caractères!");
+    }
+        alert("Formulaire valide!");
 });
+
+const choix = document.getElementById("choix"); 
+const bouton = document.getElementById("sauvegarder");//si formulaire validé 
+bouton.addEventListener("click", () => {
+    if((titre.length === true) && (annee.length === true) && (realisateur.length === true)) {
+        preventDefault();
+    } 
+    // choix.addEventListener("click", () => {}  
+    if({
+        choix = { //j'essaye de rentrer un nouveau film
+        titre.document.getElementById("titre").value
+        annee.document.getElementById("annee").value
+        realisateur.document.getElementById("realisateur").value}
+    });
+    film.push("nouveauFilm")
+    alert("film ajouté avec succès!")
+})

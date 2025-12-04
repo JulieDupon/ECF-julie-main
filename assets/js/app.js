d9b41664c92b978bdd4f8923e0d05a02fab2f6b4
JulieDupon <j.dupon18@gmail.com>
2025-12-04 16:46:38 +0100

    ecf3

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,59 +20,94 @@ let film = [
         realisateur: "Andy Muschietti"
     }
 ];
-console.log(film);
+// console.log(film);
 
-let titre = document.getElementById("titre")
-let annee = document.getElementById("annee")
-let realisateur = document.getElementById("realisateur")
+let titre = document.getElementById("title");
+let annee = document.getElementById("year");
+let realisateur = document.getElementById("author");
+let button = document.getElementById("sauvegarder");
+let tbody = document.querySelector("#film tbody");
 
+// const button = document.getElementById("ajouter");//vérification des données de l'utilisateur
 
-const button = document.getElementById("ajouter");//vérification des données de l'utilisateur
-
-button.addEventListener("click", (e) => {
+button.addEventListener("click", function(e) {
     e.preventDefault();
 
-    if(titre.value.length < 2 ){
+let anneeActuelle = new Date().getFullYear();
+let anneeValeur = parseInt(annee.value);
+
+    if(titre.value.length < 2){
         alert("Le titre doit avoir au moins 2 caractères!");
-        return false;
+        return;
     }
-    if(annee.value.length !== 4){
-        alert("L'année doit contenir que 4 chiffres!");
-        return false;
+    if (isNaN(anneeValeur) || anneeValeur < 1900 || anneeValeur > anneeActuelle){
+        alert(`L'année doit être entre 1900 et l'année actuelle!`);
+        return;
     }
-    if(realisateur.value.length < 5 ){
+    // if(annee.value.length !== 4){
+    //     alert("L'année doit contenir que 4 chiffres!");
+    //     return;
+    // }
+    if(realisateur.value.length < 5){
         alert("Le réalisateur doit avoir minimum 5 caractères!");
-        return false;
+        return;
     }
-        alert("Formulaire valide!");
-});
-
-// const choix = document.getElementById("choix"); 
-const bouton = document.getElementById("sauvegarder");//si formulaire validé 
-
-bouton.addEventListener("click", function(e) {
-    e.preventDefault();
-    
-    if(
-        titre.value.length >= 2 && 
-        annee.value.length === 4 &&
-        realisateur.value.length >= 5
-    ){
 
     let nouveauFilm = {
         titre: titre.value,
         annee: annee.value,
         realisateur: realisateur.value
-    };
+};
 
-    film.push(nouveauFilm) // Ajouter film dans le tableau
-    setTimeout("Film ajouté avec succès!", 3000) //Afficher message d'alerte pendant 3s
-    console.log(film);
+// });
+
+// const choix = document.getElementById("choix"); 
+// const bouton = document.getElementById("sauvegarder");//si formulaire validé 
+
+// bouton.addEventListener("click", function(e) {
+//     e.preventDefault();
     
-    } else {
-        setTimeout("Erreur dans le formulaire", 5000); //Afficher message d'alerte pendant 5s  
-        console.log(film);
-        
-    }  
-     
+//     if(
+//         titre.value.length >= 2 && 
+//         annee.value.length === 4 &&
+//         realisateur.value.length >= 5
+//     ){
+
+    film.push(nouveauFilm);  // Ajouter film dans le tableau
+
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${nouveauFilm.titre}</td>
+        <td>${nouveauFilm.annee}</td>
+        <td>${nouveauFilm.realisateur}</td>
+        <td><button class="btn btn-danger">supprimer</button></td>
+`;
+tbody.appendChild(tr);
+
+alert("Film ajouté avec succès!");
+console.log(film);
+
+//pour vider les input
+titre.value = "";
+annee.value = "";
+realisateur.value = "";
+
 });
+
+tbody.addEventListener("click", function(e){
+    if(e.target && e.target.tagName === "BUTTON" && e.target.textContent === "supprimer"){
+        e.target.closest("tr").remove();
+    }
+});
+
+
+
+    // setTimeout("Film ajouté avec succès!", 3000) //Afficher message d'alerte pendant 3s
+    // console.log(film);
+    
+    // } else {
+    //     setTimeout("Erreur dans le formulaire", 5000); //Afficher message d'alerte pendant 5s  
+    //     console.log(film);
+        
+    // } 
+    <button onclick="window.location.href='new.html';">ajouter</button>    
--- assets/js/new.js
+++ assets/js/new.js
@@ -3,26 +3,55 @@ let episode = document.querySelector('#episode');
 let serie = document.querySelector('#serie');
 let saison = document.querySelector('#saison');
 let button = document.querySelector('#button');
+let resultatDiv = document.querySelector('#resultat');
 
-const key = cf65b73b
+const key = "cf65b73b";
 
 
-button.addEventListener('click', function(){
-    film = film.value;
-    episode = episode.value;
-    serie = serie.value
-    saison = saison.value
+button.addEventListener('click', function() {
 
-    const f = fetch(`http://www.omdbapi.com/?=${film}&${episode}&${serie}&${saison}&apikey=${cf65b73b}&units=metric&lang=fr`)
+    const filmValue = film.value.trim(); //je rajoute le .trim pour eviter les espaces inutiles
+    const episodeValue = episode.value.trim();
+    const serieValue = serie.value.trim();
+    const saisonValue = saison.value.trim();
+
+    let url = `https://www.omdbapi.com/?apikey=cf65b73b&t=${encodeURIComponent(film)}=${episode}=${serie}=${saison}`; //pour les valeurs de paramamètres caractères spéciaux
+
+    /*je peux pas mettre 2 &t sinon ça écrase le premier*/
+    if (filmValue) url += `http://www.omdbapi.com/?apikey=cf65b73b&t=${filmValue.trim()}`; 
+    if (serieValue) url += `http://www.omdbapi.com/?apikey=cf65b73b&t=${serieValue.trim()}`;
+    if (saisonValue) url += `http://www.omdbapi.com/?apikey=cf65b73b&Season=${saisonValue.trim()}`;
+    if (episodeValue) url += `http://www.omdbapi.com/?apikey=cf65b73b&Episode=${episodeValue.trim()}`;
+
+    fetch('http://www.omdbapi.com/?t=deadpool&apikey=cf65b73b')
     .then(response => response.json())
-    .then(data => {
-        if (data.response === "true"){
-            console.log(`film : ${data.film}`);
-            console.log(`episode : ${data.episode}`);
-            console.log(`serie : ${data.serie}`);
-            console.log(`saison : ${data.saison}`);
-        }
-    })
-})
- 
-    .catch(error => console.error("erreur :", error));
+    .then(data => console.log(data))
+    .catch(error => console.error('Erreur:', error));   
+
+        if (data.Response === "True") {
+            data.Search.forEach
+        }           
+//         const card = ` 
+//             <div class="col-12 col-md-6 col-lg-4 mb-4">
+//                 <div class="card">
+//                     <img src="${poster}" class="card-img-top" alt="${item.title}">
+//                     <div class="card-body">
+//                     <h5 class="card-title">${item.title}</h5>
+//                     <p class="card-text">Année : ${item.year}</p>
+//                     <p class="card-text">Type : ${item.author}</p>
+//                     </div>
+//                 </div>
+//             </div> 
+//             `;  
+//             resultatDiv.innerHTML += card;  
+//             });
+//             console.log("Titre :", data.Title); 
+//             console.log("Année :", data.Year);
+//             console.log("Type :", data.Type);
+//         } else {
+//             console.log("Erreur");
+//         }
+//     )
+//     .catch(error => console.log(error));
+// Array.IndexOf(Search.start)
+});
